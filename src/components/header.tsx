c701815e0b6c47583a7e89e4d33690d81edec008
joao <gabriel.122.gm85@gmail.com>
2024-04-21 14:38:48 -0400

    Implementada a interface inicial.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,12 @@ import iconMain from '../assets/iconMain.svg'
  
 export function Header(){
     return (
-        <div>
+        <div className="flex items-center gap-5">
             <img src={iconMain}/>
+            <nav className="flex items-center gap-5">
+                <a href="" className="font-medium text-sm text-zinc-300">Eventos</a>
+                <a href="" className="font-medium text-sm">Participantes</a>
+            </nav>
         </div>
     )
 }
